94a58a50b1d2fb036f03e812a0474904bb6f0a74
eu casa <marcotelles123@gmail.com>
2019-10-31 23:36:54 -0300

    uinta aula

---
+++ fullstack_vue_graphql_node_5/client/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div id="slogan" class="text-center">
+      <h1>NameGator</h1>
+      <br />
+      <h6 class="text-secondary">Gerador de nomes usao Vue.js, GraphQl e Node</h6>
+    </div>
+    <AppItemList></AppItemList>
+    <div id="main">
+      <DomainList></DomainList>
+    </div>
+  </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
+import DomainList from "../src/components/DomainList";
+export default {
+  name: "app",
+  components: {
+    DomainList
+  },
+};
+</script>
+
+<style>
+#slogan {
+  margin-bottom: 30px;
+  margin-top: 30px;
+}
+#main {
+  background-color: #f1f1f1;
+  padding-bottom: 30px;
+  padding-top: 30px;
+}
+</style>
